07cb3c86a31d7383bf7277fe77e11b3fd2cf8799
James Le Poidevin <jameslepoidevin@yahoo.com>
2020-11-04 15:57:28 +0100

    start file works

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 start "processing" "P:/processing-3.5.4\processing.exe" "F:\Documents\Cours\3A_SRI\IHM-multi\Projet\Projet-IHM\palette\palette.pde"
 cd grammar
-call "sra_on.bat"
+start "sra" "sra_on.bat"
 cd ../icar
-call "Icarivy.bat"
+start "Icar" "Icarivy.bat"
 
 exit
