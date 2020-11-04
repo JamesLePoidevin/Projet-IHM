feb25245ee97ffe069f2bfb58ee849ab118dcdb0
James Le Poidevin <jameslepoidevin@yahoo.com>
2020-11-04 15:10:42 +0100

    changed start.bat file to add icar

--- start.bat
+++ start.bat
@@ -1,9 +1,7 @@
-@echo off
-
 start "processing" "P:/processing-3.5.4\processing.exe" "F:\Documents\Cours\3A_SRI\IHM-multi\Projet\Projet-IHM\palette\palette.pde"
 cd grammar
-sra_on.bat
-cd ..
-cd icar
+call "sra_on.bat"
+cd ../icar
+call "Icarivy.bat"
 
 exit
